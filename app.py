db3c47ee1fe66aca67a76a76089f41cd657568a8
Jeewana Kasun Siriwardana <64886377+jeewana-kasun@users.noreply.github.com>
2025-03-17 16:27:57 +0530

    Update app.py to enable redis

--- app.py
+++ app.py
@@ -4,8 +4,20 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello():
-    count = '1'
+    count = 1
     return f'Hello World! I have been seen {count} times.\n'
